b1e2af04f358b57fefdc8fab91ff7688c35fee01
Mydev8999 <124028093+Mydev8999@users.noreply.github.com>
2023-02-12 19:37:25 +0100

    code de botdev

---
+++ bot dev apprendre.py
@@ -0,0 +1,108 @@
+#maintien la fenetre ouverte
+while True:
+    
+    print("§ *     $$$$$  #         #    #####   ######   #####")
+    print("§   *   $       #       #     #   #   #    #     #")
+    print("§    *  $$$$$    #     #      ####    #    #     #")
+    print("§   *   $         #   #       #   #   #    #     #")
+    print("§ **    $$$$$      ###        #####   ######     #")
+    print("\n\nAVERTISSEMENT! il est consayer de garder cette fenetre ouverte car ils n'y a pas de sauvegarde de votre aprentisage! ")
+    nom = input("quel est ton nom: ")
+    print("bonjour, " + nom)
+
+    print("chargement...")
+
+    print("bienvenu(e) dans dans ton bot pour apprendre le dev web  ")
+
+    print("\ncommençon par les base: ")
+
+    print("Le dev web est constituer de trois language il y en a plus mes nous allons rester sur les base.")
+    print("connais tu le language de base pour apprendre le dev web.")
+    html = input("va si: ")
+    #verifie si le texte entrer est bon
+    if html == "html":
+        print("bien jouer")
+    else:
+
+        print("non c'était le html et pas " + html)
+
+    print("\nbon maintenant que tu connais le language de base on va pouvoir commencer")
+    
+    print("\nPour commencer nous allons aprendre les base ")
+    print("\nsi vous utiliser un IDE comme vs code ecriver '!' dans l'éditeur et la structure html se générera automatiquement")
+    print("\ncommençon pour afficher un titre taper 'h1' a l'interieur de la balise body ")
+    print("\ncomme sa:")
+    print("\n<h1>bonjour</h1>")
+    titre = input("a ton tour: ")
+    #verifie si le texte entrer est bon
+    if titre == "<h1>bonjour</h1>":
+        print("\ntrès bien")
+
+    else:
+
+        print("\nrater c'était '<h1>bonjour</h1>' ")
+      
+
+    print("bien sur vous pouver mettre tous se que vous vouler mais ici on ecris bonjour\n ")
+
+    print("bon pour le titre c'est bon maintenant on va ecrire des paragraphe\n ")
+
+
+    print("\npour créer un paragraphe il faux écrire <p>lorem40</p> la encore vous pouver mettre tous se que vous vouler mais nous mettons 'lorem40' pour générer 40\n ligne aléatoire ")
+    print("\n a ton tour:")
+    p = input("ici: ")
+
+    if p == "<p>lorem40</p>":
+
+        print("super")
+    else:
+
+        print("oups rater c'était <p>lorem40</p>")
+
+    print("\nbien maintenant nous allons créer un sous titre pour sela nous allon écrire <h2>sous titre</h2> ah oui plus le nombre après est grand plus les lettre son petite")
+
+
+    sousTitre = input("a ton tour: ")
+
+    if sousTitre == "<h2>sous titre</h2>":
+        print("bien jouer")
+
+    else:
+
+        print("oups raté c'était <h2>sous titre</h2>")
+
+    print('bien maintenant nous allons inserer une image sur votre site pour sela  ils faut mettre une image dans le dossier ou votre code est ecrit ensuite ons ecris <img src="nom de votre image">')
+    nomDeLimage = input("donne le nom de ton image: ")
+    img = input("a ton tour: ")
+
+    if img == '<img src="' + nomDeLimage + '">':
+
+        print("bien jouer")
+
+    else:
+
+        print("ho domage c'est raté")
+
+    print("ésite pas a t'abonner a la chaine youtube tutoRoblox")
+
+    print("A PLUS!")
+
+    print("FIN")
+
+    
+
+
+
+
+
+
+
+
+
+#A TERMINER
+
+
+
+
+
+
